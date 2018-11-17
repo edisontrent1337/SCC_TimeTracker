2e0e52079b1635fd8d83f1368ac71e53583cd549
Sinthujan <sthana@devboost.de>
2018-11-17 16:50:48 +0100

    Removed ui project from user-service.

--- user-service/user-service.ui/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import authReducer from '../reducers/authentication.reducer.js';
-import signupReducer from '../reducers/signup.reducer.js';
-import thunkMiddleware from 'redux-thunk';
-const rootReducer = combineReducers({authReducer, signupReducer});
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-
-export default store;
--- user-service/user-service.ui/src/utils/UrlUtils.js
+++
@@ -1,9 +0,0 @@
-export default class UrlUtils {
-    static addAppBaseUrl(path) {
-        if (typeof process.env.REACT_APP_BASE_URL === "undefined") {
-            return path;
-        }
-
-        return process.env.REACT_APP_BASE_URL + path;
-    }
-}
