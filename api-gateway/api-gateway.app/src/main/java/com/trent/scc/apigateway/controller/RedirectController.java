f0231b5806d159d36e25c9fa0526df7da0154e0d
Sinthujan Thanabalasingam (DevBoost) <sthana@devbooost.de>
2019-01-25 22:45:17 +0100

    Added redirects for frontend

--- api-gateway/api-gateway.app/src/main/java/com/trent/scc/apigateway/controller/RedirectController.java
+++ api-gateway/api-gateway.app/src/main/java/com/trent/scc/apigateway/controller/RedirectController.java
@@ -8,8 +8,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class RedirectController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String signUp() {
-		System.out.println("sdsfdsfdsfdsfsdfdsfsdfsdjnsdklfjhsdklfhjlsdj");
+	public String index() {
+		System.out.println("Redirect to /");
 		return "forward:/ui/";
 	}
+
+	@RequestMapping(value = "/activities")
+	public String inPlace() {
+		System.out.println("Redirect to activities");
+		return "forward:/";
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signUp() {
+		System.out.println("Redirect to signup");
+
+		return "forward:/";
+	}
+
+	@RequestMapping(value = "/dashboard/{path:[^\\.]+}/**")
+	public String dashboard() {
+		return "forward:/";
+	}
+
+	@RequestMapping(value = "/dashboard")
+	public String dashboardInPlace() {
+		return "forward:/";
+	}
+}
 }
