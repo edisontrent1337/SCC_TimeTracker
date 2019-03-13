56c0a285eae8c4a76e0a9cf66716e0e76f424bd0
Sinthujan Thanabalasingam (DevBoost) <sthana@devbooost.de>
2019-03-13 21:14:52 +0100

    Added a progress bar.

--- python-test/python-test.ui/src/screens/ProgressScreen.js
+++ python-test/python-test.ui/src/screens/ProgressScreen.js
@@ -3,7 +3,9 @@ import {client} from "../client/APIClient";
 import Hint from "../web-react/hints/Hint";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
-
+import LoadingIndicator from "../web-react/indicators/LoadingIndicator";
+import {ProgressBar} from "react-bootstrap";
+import "./test.css";
 export default class ProgressScreen extends React.Component {
 	constructor(props) {
 		super(props);
@@ -18,34 +20,49 @@ export default class ProgressScreen extends React.Component {
 			.then(res => res.json())
 			.then(res => {
 				this.setState({
-					progress: res.data[0]
+					progress: res.data[0],
 				});
 			});
 	}
 
 	render() {
-		return (
-			<div className={"container"}>
-				<Header/>
-				<Hint>
-					{this.state.progress &&
-					<div>
-						<h1>
-							{Math.round((this.state.progress.studentsThatAnswered / this.state.progress.totalStudents) * 100)}
-							% of you have already answered!
-						</h1>
-						<h1>
-							{this.state.progress.studentsThatAnswered} of you have done it!
-						</h1>
-						<h1>
-							{this.state.progress.totalStudents - this.state.progress.studentsThatAnswered} are still
-							missing!
-						</h1>
-					</div>}
-				</Hint>
-				<Footer/>
-			</div>
-		);
+		if (!this.state.progress) {
+			return (
+				<div className={"container"}>
+					<Header/>
+					<LoadingIndicator width={128} height={128}/>
+					<Footer/>
+				</div>
+			);
+		}
+		else {
+			let {studentsThatAnswered, totalStudents} = this.state.progress;
+			let progress = Math.round((studentsThatAnswered / totalStudents) * 100);
+			return (
+				<div className={"container"}>
+					<Header/>
+					<Hint>
+						{this.state.progress &&
+						<div>
+							<h1>
+								{totalStudents} are enrolled this year.
+							</h1>
+							<h1>
+								{progress} % of you have already answered!
+							</h1>
+							<h1>
+								{studentsThatAnswered} of you have done it!
+							</h1>
+							<h1>
+								{totalStudents - studentsThatAnswered} are still
+								missing!
+							</h1>
+						</div>}
+					</Hint>
+					<ProgressBar striped animated variant="info" now={progress}/>
+					<Footer/>
+				</div>
+			);
+		}
 	}
-
 }
